1aab98eaddf200f90c041ed1c1b60df8a69095a1
Epona <datouhaoruijie@gmail.com>
2024-07-21 15:21:15 +0900

    Disable swapfile

--- init.lua
+++ init.lua
@@ -17,6 +17,8 @@ vim.opt.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+vim.o.swapfile = false
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
