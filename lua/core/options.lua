f31c2adf9203a2e3b97cb34e5d880425164dedaf
Epona <datouhaoruijie@gmail.com>
2024-12-05 22:21:50 +0900

    restructure

---
+++ lua/core/options.lua
@@ -0,0 +1,46 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+
+vim.o.swapfile = false
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: For more options, you can see `:help option-list`
+
+local opt = vim.opt
+
+opt.number = true
+opt.mouse = 'a'
+opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
+
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = 'yes'
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.inccommand = 'split'
+opt.cursorline = true
+opt.scrolloff = 10
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
