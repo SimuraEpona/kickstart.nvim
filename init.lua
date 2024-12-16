4945540228d98ed1a6c3e99de844e7ce63e306c0
Epona <datouhaoruijie@gmail.com>
2024-12-17 07:50:37 +0900

    remove lua/core

--- init.lua
+++ init.lua
@@ -1,9 +1,31 @@
 vim.g.mapleader = ' '
 vim.g.have_nerd_font = true
 
-require 'core.autocmds'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
-require 'core.lazy'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+  spec = { import = 'plugins' },
+  rocks = { enabled = false },
+  checker = { notify = false },
+  change_detection = { notify = false },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
