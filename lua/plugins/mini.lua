d576ca04c631ce942382c181f36eefc8999c1c87
Epona <datouhaoruijie@gmail.com>
2024-12-27 08:05:36 +0900

    remove space

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,9 +8,7 @@ return {
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
-
     require('mini.indentscope').setup()
-
     require('mini.diff').setup()
 
     local statusline = require 'mini.statusline'
