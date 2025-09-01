8775ee20e33d4cec62f316f91ab8fdc5e68e7019
Epona <datouhaoruijie@gmail.com>
2025-09-01 19:34:15 +0900

    mini.notify

--- init.lua
+++ init.lua
@@ -155,9 +155,6 @@ require('lazy').setup({
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      { 'j-hui/fidget.nvim', opts = {} },
-
       'saghen/blink.cmp',
     },
     config = function()
